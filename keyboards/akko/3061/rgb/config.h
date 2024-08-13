f24bf2666511dd9086ca07a0391b986a53359b0f
HorrorTroll <sonicvipduc@gmail.com>
2024-08-13 17:18:51 +0700

    Added akko 3061 rgb keyboard

---
+++ keyboards/akko/3061/rgb/config.h
@@ -0,0 +1,13 @@
+// Copyright 2024 HorrorTroll <https://github.com/HorrorTroll>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SN32_RGB_MATRIX_ROW_PINS {B15, B14, B13, C1, C0, C15, C5, C4, C3, C8, C7, C6, C11, C10, C9}
+
+#define COMBO_TERM_PER_COMBO
+#define COMBO_MUST_PRESS_IN_ORDER_PER_COMBO
+
+// Debug Settings
+#define DEBUG_ENABLE
+#define DEBUG_MATRIX_SCAN_RATE
