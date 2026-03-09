74331dc7c0e3b73ab1d8ff46bd25667f18614a1d
HorrorTroll <sonicvipduc@gmail.com>
2026-03-09 15:53:52 +0700

    Added legacy ChibiOS USB endpoints before async for SN32F260

--- tmk_core/protocol/chibios/chibios.mk
+++ tmk_core/protocol/chibios/chibios.mk
@@ -2,12 +2,19 @@ PROTOCOL_DIR = protocol
 CHIBIOS_DIR = $(PROTOCOL_DIR)/chibios
 
 
+ifneq ($(filter $(MCU_SERIES),SN32F260),)
+SRC += $(CHIBIOS_DIR)/usb_legacy/usb_main.c
+SRC += $(CHIBIOS_DIR)/usb_legacy/chibios.c
+SRC += usb_descriptor.c
+SRC += $(CHIBIOS_DIR)/usb_legacy/usb_driver.c
+else
 SRC += $(CHIBIOS_DIR)/usb_main.c
 SRC += $(CHIBIOS_DIR)/chibios.c
 SRC += usb_descriptor.c
 SRC += $(CHIBIOS_DIR)/usb_driver.c
 SRC += $(CHIBIOS_DIR)/usb_endpoints.c
 SRC += $(CHIBIOS_DIR)/usb_report_handling.c
+endif
 SRC += $(CHIBIOS_DIR)/usb_util.c
 SRC += $(LIBSRC)
 
